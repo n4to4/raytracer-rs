aa40fb8f0333bcc17d4bb2fc6a63ade4062f777a
n4to4 <n4to4k@gmail.com>
2022-01-05 10:13:07 +0900

    first ppm

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
+const IMAGE_WIDTH: usize = 256;
+const IMAGE_HEIGHT: usize = 256;
+
 fn main() {
-    let mut counter = 0;
-    loop {
-        std::thread::sleep(std::time::Duration::from_secs(1));
-        println!("{}", counter);
-        counter+=1;
+    println!("P3");
+    println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
+    println!("{}", 255);
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+        for i in 0..IMAGE_WIDTH {
+            let r = (i as f64) / (IMAGE_WIDTH - 1) as f64;
+            let g = (j as f64) / (IMAGE_HEIGHT - 1) as f64;
+            let b = 0.25;
+
+            let ir = (255.999 * r) as i64;
+            let ig = (255.999 * g) as i64;
+            let ib = (255.999 * b) as i64;
+
+            println!("{} {} {}", ir, ig, ib);
+        }
     }
 }
