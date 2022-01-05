2b5eaf2f25997c60337bdada2c02d3d5c0316945
n4to4 <n4to4k@gmail.com>
2022-01-05 10:30:37 +0900

    progress indicator

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
+use std::io::Write;
+
 const IMAGE_WIDTH: usize = 256;
 const IMAGE_HEIGHT: usize = 256;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("P3");
     println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
     println!("{}", 255);
 
+    let stderr = std::io::stderr();
+    let mut handle = stderr.lock();
     for j in (0..IMAGE_HEIGHT).rev() {
+        write!(handle, "\rScanlines remaining: {}", j)?;
+        handle.flush()?;
         for i in 0..IMAGE_WIDTH {
             let r = (i as f64) / (IMAGE_WIDTH - 1) as f64;
             let g = (j as f64) / (IMAGE_HEIGHT - 1) as f64;
@@ -19,4 +25,6 @@ fn main() {
             println!("{} {} {}", ir, ig, ib);
         }
     }
+    write!(handle, "\nDone.\n")?;
+    Ok(())
 }
