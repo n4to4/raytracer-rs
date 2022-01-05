076fb968b6dad9b28fc32296c1e20cc7c26fff5b
n4to4 <n4to4k@gmail.com>
2022-01-05 11:16:34 +0900

    perf

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,25 @@ const IMAGE_WIDTH: usize = 256;
 const IMAGE_HEIGHT: usize = 256;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("P3");
-    println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
-    println!("{}", 255);
-
     let stderr = std::io::stderr();
-    let mut handle = stderr.lock();
+    let mut stderr = stderr.lock();
+    let stdout = std::io::stdout();
+    let mut stdout = stdout.lock();
+
+    write!(stdout, "P3")?;
+    write!(stdout, "{} {}", IMAGE_WIDTH, IMAGE_HEIGHT)?;
+    write!(stdout, "{}", 255)?;
+
     for j in (0..IMAGE_HEIGHT).rev() {
-        write!(handle, "\rScanlines remaining: {}", j)?;
-        handle.flush()?;
+        write!(stderr, "\rScanlines remaining: {}", j)?;
+        stderr.flush()?;
         for i in 0..IMAGE_WIDTH {
             let r = (i as f64) / (IMAGE_WIDTH - 1) as f64;
             let g = (j as f64) / (IMAGE_HEIGHT - 1) as f64;
             let b = 0.25;
-            println!("{}", Vec3::new(r, g, b));
+            write!(stdout, "{}", Vec3::new(r, g, b))?;
         }
     }
-    write!(handle, "\nDone.\n")?;
+    write!(stderr, "\nDone.\n")?;
     Ok(())
 }
