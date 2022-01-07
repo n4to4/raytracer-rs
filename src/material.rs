149498821f1b12d8c92bf8b246112d42b43af322
n4to4 <n4to4k@gmail.com>
2022-01-08 08:19:01 +0900

    10.4

--- src/material.rs
+++ src/material.rs
@@ -22,7 +22,8 @@ impl Material for Dielectric {
         let sin_theta = (1.0 - cos_theta.powi(2)).sqrt();
 
         let cannot_refract = refraction_ratio * sin_theta > 1.0;
-        let direction = if cannot_refract {
+        let direction = if cannot_refract || reflectance(cos_theta, refraction_ratio) > random_f64()
+        {
             unit_direction.reflect(rec.normal)
         } else {
             unit_direction.refract(rec.normal, refraction_ratio)
@@ -31,4 +32,11 @@ impl Material for Dielectric {
         let scattered = Ray::new(rec.p, direction);
         Some((attenuation, scattered))
     }
+}
+
+fn reflectance(cosine: f64, ref_idx: f64) -> f64 {
+    // Use Schlick's approximation for reflectance.
+    let r0 = (1.0 - ref_idx) / (1.0 + ref_idx);
+    let r0 = r0.powi(2);
+    r0 + (1.0 - r0) * (1.0 - cosine).powi(5)
 }
