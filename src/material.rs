3dc8ae964b81871013b9a0af8c56a42e4afbc1c9
n4to4 <n4to4k@gmail.com>
2022-01-08 08:13:20 +0900

    10.3

--- src/material.rs
+++ src/material.rs
@@ -18,9 +18,17 @@ impl Material for Dielectric {
         };
 
         let unit_direction = Vec3::unit_vector(r_in.direction);
-        let refracted = unit_direction.refract(rec.normal, refraction_ratio);
+        let cos_theta = Vec3::dot(-unit_direction, rec.normal).min(1.0);
+        let sin_theta = (1.0 - cos_theta.powi(2)).sqrt();
 
-        let scattered = Ray::new(rec.p, refracted);
+        let cannot_refract = refraction_ratio * sin_theta > 1.0;
+        let direction = if cannot_refract {
+            unit_direction.reflect(rec.normal)
+        } else {
+            unit_direction.refract(rec.normal, refraction_ratio)
+        };
+
+        let scattered = Ray::new(rec.p, direction);
         Some((attenuation, scattered))
     }
 }
