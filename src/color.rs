eb303814c627efa7305931cabbb568f594733628
n4to4 <n4to4k@gmail.com>
2022-01-07 10:46:01 +0900

    gamma correction

--- src/color.rs
+++ src/color.rs
@@ -8,11 +8,12 @@ impl Vec3 {
         let g = &mut self.y;
         let b = &mut self.z;
 
-        // Divide the color by the number of samples.
+        // Divide the color by the number of samples
+        // and gamma-correct for gamma=2.0.
         let scale = 1.0 / samples_per_pixel as f64;
-        *r *= scale;
-        *g *= scale;
-        *b *= scale;
+        *r = (scale * *r).sqrt();
+        *g = (scale * *g).sqrt();
+        *b = (scale * *b).sqrt();
 
         Color(self)
     }
