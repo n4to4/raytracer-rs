d51c71a8552ba48ca1c1d4768fbd0bf38cbce13c
n4to4 <n4to4k@gmail.com>
2022-01-07 09:32:20 +0900

    camera class

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![allow(clippy::new_without_default)]
 
+pub mod camera;
 pub mod hittable;
 pub mod hittable_list;
 pub mod ray;
@@ -16,4 +17,10 @@ pub use vec3::Vec3;
 
 pub fn degrees_to_radians(degrees: f64) -> f64 {
     degrees * std::f64::consts::PI / 180.0
+}
+
+pub fn random_f64() -> f64 {
+    use rand::Rng;
+    let mut rng = rand::thread_rng();
+    rng.gen_range(0.0..1.0)
 }
