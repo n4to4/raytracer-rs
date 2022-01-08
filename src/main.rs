5c81c2feaa201371a0fd4e18ad8113afe5a21fd1
n4to4 <n4to4k@gmail.com>
2022-01-08 09:19:36 +0900

    12.2

--- src/main.rs
+++ src/main.rs
@@ -47,12 +47,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     world_add((1.0, 0.0, -1.0), 0.5, material_right);
 
     // Camera
+    let lookfrom = Vec3::new(3.0, 3.0, 2.0);
+    let lookat = Vec3::new(0.0, 0.0, -1.0);
+    let dist_to_focus = (lookfrom - lookat).length();
     let camera = Camera::new(
-        Vec3::new(-2.0, 2.0, 1.0),
-        Vec3::new(0.0, 0.0, -1.0),
+        lookfrom,
+        lookat,
         Vec3::new(0.0, 1.0, 0.0),
         20.0,
         ASPECT_RATIO,
+        2.0,
+        dist_to_focus,
     );
 
     // Stdout/err
--- src/vec3.rs
+++ src/vec3.rs
@@ -76,6 +76,20 @@ impl Vec3 {
         }
     }
 
+    pub fn random_in_unit_disk() -> Self {
+        loop {
+            let p = Vec3::new(
+                random_range_f64(-1.0, 1.0),
+                random_range_f64(-1.0, 1.0),
+                0.0,
+            );
+            if p.length_squared() >= 1.0 {
+                continue;
+            }
+            return p;
+        }
+    }
+
     pub fn random_unit_vector() -> Self {
         Vec3::unit_vector(Vec3::random_in_unit_sphere())
     }
